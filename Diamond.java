324872be91d4ca92e9e6a374c14f20eff6f45f06
Anthowill <awilliams3647@gmail.com>
2024-10-07 18:09:15 -0400

    Basic if statement formed and starting for loops

--- Diamond.java
+++ Diamond.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int enteredDiamondSize, remainderDiamondSize;
+    enteredDiamondSize = scan.nextInt();
+    remainderDiamondSize= enteredDiamondSize % 2;
+
+    if(remainderDiamondSize == 0){
+      for(int index=enteredDiamondSize; index > 0; index--){
+        System.out.print(" ");
+      }
+
+      for(int index=0; index < enteredDiamondSize; index++){
+        System.out.print("*");
+      }
+    }
   }
 }
