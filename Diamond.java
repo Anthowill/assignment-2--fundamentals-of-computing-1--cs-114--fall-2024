4e0ff014c7fbb156bf53e4f16fbe3bc2d3b7092e
Anthowill <awilliams3647@gmail.com>
2024-10-07 18:46:40 -0400

    Top half printing correctly

--- Diamond.java
+++ Diamond.java
@@ -3,17 +3,21 @@ import java.util.Scanner;
 public class Diamond {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    int enteredDiamondSize, remainderDiamondSize;
+    int enteredDiamondSize, remainderDiamondSize,numberOfSpaces;
     enteredDiamondSize = scan.nextInt();
-    remainderDiamondSize= enteredDiamondSize % 2;
+    remainderDiamondSize = enteredDiamondSize % 2;
+    numberOfSpaces = enteredDiamondSize/2+1;
 
     if(remainderDiamondSize == 0){
-      for(int index=enteredDiamondSize; index > 0; index--){
-        System.out.print(" ");
-      }
-
-      for(int index=0; index < enteredDiamondSize; index++){
-        System.out.print("*");
+      for(int topRows=0; topRows <= enteredDiamondSize/2; topRows++){
+        for(int spaceindex = numberOfSpaces; spaceindex > 0;  spaceindex--){
+          System.out.print(" ");
+        }
+        numberOfSpaces--;
+        for(int symbolIndex=0; symbolIndex < 2*topRows-1; symbolIndex++){
+          System.out.print("*");
+        }
+        System.out.println();
       }
     }
   }
